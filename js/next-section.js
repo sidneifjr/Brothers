b4c1364e63cd843e892544cd36fe8432339a3c79
sidneifjr <sidneijr.77@gmail.com>
2016-10-26 11:05:13 -0200

    Brothers 2.0
    Criado com HTML, CSS e Bootstrap, a partir do zero. Ainda vou terminar a
    estrutura do layout de algumas áreas e responsividade...

---
+++ js/next-section.js
@@ -0,0 +1,17 @@
+	/* Next Section   
+	-------------------------------------------------------------------*/
+	$('.next-section .go-to-about').click(function() {
+    	$('html,body').animate({scrollTop:$('#about').offset().top}, 1000);
+  	});
+  	$('.next-section .go-to-subscribe').click(function() {
+    	$('html,body').animate({scrollTop:$('#subscribe').offset().top}, 1000);
+  	});
+  	$('.next-section .go-to-contact').click(function() {
+    	$('html,body').animate({scrollTop:$('#contact').offset().top}, 1000);
+  	});
+  	$('.next-section .go-to-page-top').click(function() {
+    	$('html,body').animate({scrollTop:$('#page-top').offset().top}, 1000);
+  	});
+
+  	/* Next Section End
+	-------------------------------------------------------------------*/
