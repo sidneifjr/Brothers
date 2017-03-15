d10834e4e1c28f26269bdbae4527cff6f14d3d53
sidneifjr <sidneijr.77@gmail.com>
2017-03-15 16:48:13 -0300

    Polishing
    Did some work around bits of code, now it makes a bit more sense
    semantically. Also added some small things, not really a big deal.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,95 @@
-// botões de eventos
+
+// Botões de eventos
+
 $(function(){
+
 		$('.evento-buttonid').on('click', function(){
+
 			var buttonId = $(this).attr('data-button');
+
 			$('.'+buttonId).toggle(200);
+
 	});
+
+});
+
+$('.box-exterior').hover(
+
+	function(){
+
+		$(this).find('.caption').fadeToggle(300);
+
+});
+
+$('.box-exterior-2').hover(
+
+	function(){
+
+		$(this).find('.caption').fadeToggle(300);
+
+});
+
+// Validação de formulários
+
+function isEmail(email) {
+
+	var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+	return regex.test(email);
+
+}
+
+$("#submit").click(function(){
+
+	var error = "";
+
+	var missing = "";
+
+	if ($("#nome").val() == "") {
+
+		missing += "<br> Nome";
+
+	}
+
+	if ($("#assunto").val() == ""){
+
+		missing += "<br> Assunto";
+
+	}
+
+	if ($("#email").val() == "") {
+
+		missing += "<br> Email";
+
+	}
+
+	if ($("#mensagem").val() == "") {
+
+		missing += "<br> Mensagem vazia!";
+
+	}
+
+	if (missing != "") {
+
+		error += "<p> Os campos seguintes estão vazios: </p>" + missing;
+
+	}
+
+	if (isEmail($("#email").val()) == false) {
+
+		error += "<p> Seu email é inválido </p>";
+
+	}
+
+	if (error != "") {
+
+		$(".alert-danger").html(error);
+
+	} else {
+
+		$(".alert-success").show();
+		$(".alert-danger").hide();
+
+	}
+
 });
